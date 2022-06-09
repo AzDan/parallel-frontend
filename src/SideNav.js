87f4f2ababb7d66c7f9aa8330ee2d99235986567
AzDan <vrocks98@gmail.com>
2022-06-09 09:28:19 +0530

    initial page layout setup

---
+++ src/SideNav.js
@@ -0,0 +1,19 @@
+import './styles/sidenav.scss';
+
+function SideNav() {
+  return(
+    <div className='side-nav'>
+      <div className='user-deatils'>
+        Dashboard
+      </div>
+      <div className=''>
+        Random Dataset from API
+      </div>
+      <div className='navigation'>
+        Problem 1
+      </div>
+    </div>
+  );
+}
+
+export default SideNav;
